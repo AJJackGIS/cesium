c77e2458c87178212675dbd26fb50394437f0881
AJJackGIS <kafei.aojie2012@gmail.com>
2025-02-26 10:28:57 +0800

    更新amap文档

--- packages/engine/Source/Scene/AMapImageryProvider.js
+++ packages/engine/Source/Scene/AMapImageryProvider.js
@@ -5,13 +5,15 @@ const TILE_URL = {
   img: "//webst{s}.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",
   cia: "//webst{s}.is.autonavi.com/appmaptile?style=8&x={x}&y={y}&z={z}",
   vec: "//webst{s}.is.autonavi.com/appmaptile?style=9&x={x}&y={y}&z={z}",
+  vec_b: "//webst{s}.is.autonavi.com/appmaptile?style=7&x={x}&y={y}&z={z}",
 };
 
 /**
- * 高德地图ImageryProvider
- * @param {string} url 自定义链接
- * @param {string} crs WGS84
- * @param {string} style 地图类型 img:影像地图  vec:电子地图 cia:电子注记
+ * 高德地图 ImageryProvider
+ * @param {object} [options]
+ * @param {string} [options.url] 瓦片链接
+ * @param {string} [options.crs='gcj02'] scheme 默认：gcj02 纠偏：wgs84
+ * @param {string} [options.style='img'] 地图类型 img:影像地图 vec:电子地图 vec_b:电子地图大字版 cia:电子注记
  */
 class AMapImageryProvider extends UrlTemplateImageryProvider {
   constructor(options = {}) {
